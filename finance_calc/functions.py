53eabcb16eeed18b4a080b9342a2d35c57f89a64
mithun-jigsaw <mithun.shenoy@jigsaw.co.in>
2024-03-06 14:18:05 +0530

    fixed gst

--- finance_calc/functions.py
+++ finance_calc/functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_direct_formulae():
     return {
-        'Net Sales': 'Sales+GST+Other Income',
+        'Net Sales': 'Sales+Taxes Collected+Other Income',
         'Gross Profit': 'Net Sales-Cost of Goods Sold',
         'Gross Profit %': '(Gross Profit/Sales)*100',
         'CM1': 'Gross Profit-Shipping Cost-Packaging Cost',
